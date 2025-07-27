2a042b39f65993d1c8d457951f764a7ee9cf97ac
danie <daniel.ssouza.f@gmail.com>
2025-07-27 19:06:34 -0300

    Cartas: tipos e temas

---
+++ backend/src/main/java/com/media/noesis/enums/CardTheme.java
@@ -0,0 +1,7 @@
+package com.media.noesis.enums;
+
+public enum CardTheme {
+    ED1,
+    ED2,
+    REDES,
+}
---
+++ backend/src/main/java/com/media/noesis/enums/CardType.java
@@ -0,0 +1,6 @@
+package com.media.noesis.enums;
+
+public enum CardType {
+    ANSWER,
+    CHALLENGE
+}
